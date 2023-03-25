97ea343bb64752be0b3c1363491a94e602614bdb
AndriiKot <a_kocyuba@ukr.net>
2023-03-25 23:51:31 +0100

    Math methods

--- main.js
+++ main.js
@@ -1,2 +1,18 @@
 ﻿console.log(`Text
  on 2 line`)
+
+let a = 7;
+console.log(`a = ${a}`);
+
+console.log("Text\non 2 line " + a);
+console.log(Math.random())
+
+console.log(Math.max(3,4,0,1,7,2))
+
+console.log(Math.pow(16,0.5))
+console.log(Math.pow(4,2))
+console.log(Math.pow(3,3))
+
+console.log(Math.sqrt(64))
+console.log(Math.sqrt(25))
+console.log(Math.sqrt(7))
