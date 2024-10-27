acf265c0c9491efeff919a54d6b2e5fcf5d52e2a
AndriiKot <a_kocyuba@ukr.net>
2024-10-27 22:01:50 +0100

    English  added new lessons 14

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const phrasesContainer = document.getElementById("phrases");
 
 // Функция для загрузки переводов из JSON файла
 async function loadTranslations() {
-  const response = await fetch("A0-L12.json");
+  const response = await fetch("A0-L14.json");
   const translations = await response.json();
   populatePhrases(translations);
 }
