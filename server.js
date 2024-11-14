c092e1a90520036d26eaad14d4f40857e47ddf8d
AndriiKot <a_kocyuba@ukr.net>
2024-11-14 12:29:58 +0100

    App: 'English Lessons' ! added husky eslint prettier !

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
-require("dotenv").config();
-const express = require("express");
-const path = require("node:path");
-const fs = require("node:fs/promises");
+require('dotenv').config();
+const express = require('express');
+const path = require('node:path');
+const fs = require('node:fs/promises');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.set("view engine", "pug");
-app.set("views", path.join(__dirname, "views"));
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "views", "index.html"));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
-const validLevels = ["A0", "A1", "A2", "B1", "B2", "C1", "C2"];
+const validLevels = ['A0', 'A1', 'A2', 'B1', 'B2', 'C1', 'C2'];
 
 const isValidLesson = (lesson, startRang = 1, endRang = 50) => {
   const parsedLesson = parseInt(lesson);
@@ -28,25 +28,20 @@ const isValidLesson = (lesson, startRang = 1, endRang = 50) => {
   return flag;
 };
 
-app.get("/:level", (req, res) => {
+app.get('/:level', (req, res) => {
   const level = req.params.level;
 
   if (validLevels.includes(level)) {
-    res.render("lessons", { level: level });
+    res.render('lessons', { level: level });
   } else {
-    res.status(404).send("Уровень не найден");
+    res.status(404).send('Уровень не найден');
   }
 });
 
 async function getTranslations(level, lesson) {
   try {
-    const filePath = path.join(
-      __dirname,
-      "data",
-      level,
-      `${level}-L${lesson}.json`
-    );
-    const data = await fs.readFile(filePath, "utf8");
+    const filePath = path.join(__dirname, 'data', level, `${level}-L${lesson}.json`);
+    const data = await fs.readFile(filePath, 'utf8');
     return JSON.parse(data);
   } catch (error) {
     console.error(`Ошибка чтения файла: ${error}`);
@@ -54,26 +49,26 @@ async function getTranslations(level, lesson) {
   }
 }
 
-app.get("/:level/:lesson", async (req, res) => {
+app.get('/:level/:lesson', async (req, res) => {
   const { level, lesson } = req.params;
 
   if (validLevels.includes(level)) {
     if (isValidLesson(lesson)) {
       const translations = await getTranslations(level, lesson);
       if (Object.keys(translations).length === 0) {
-        res.render("developingLesson");
+        res.render('developingLesson');
       } else {
-        res.render("lesson", {
+        res.render('lesson', {
           level: level,
           lesson: lesson,
           translations: translations,
         });
       }
     } else {
-      res.status(404).send("Неверный номер урока");
+      res.status(404).send('Неверный номер урока');
     }
   } else {
-    res.status(404).send("Уровень не найден");
+    res.status(404).send('Уровень не найден');
   }
 });
 
