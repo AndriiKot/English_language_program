a1fba9c07c314dc8f4fee53eb145688619cb70ba
AndriiKot <a_kocyuba@ukr.net>
2024-11-16 17:09:28 +0100

    App: 'English Lessons' ! added lesson 26 !

--- scripts/task-scripts/manage-package-version/manage-package-version.cjs
+++ scripts/task-scripts/manage-package-version/manage-package-version.cjs
@@ -2,21 +2,6 @@ const fs = require('node:fs');
 const path = require('node:path');
 const { execSync } = require('node:child_process');
 
-// const scriptPath = './update-version.cjs';
-
-// try {
-//     const tagName = execSync('git describe --tags --abbrev=0').toString().trim();
-//     if (!tagName) {
-//         console.error('Теги не найдены!');
-//         process.exit(1);
-//     }
-
-//     execSync(`node ${scriptPath} ${tagName}`, { stdio: 'inherit' });
-// } catch (error) {
-//     console.error(`Ошибка: ${error.message}`);
-//     process.exit(1);
-// }
-
 const packageJsonPath = path.join(__dirname, '../../../package.json');
 const versionTag = execSync('git describe --tags --abbrev=0').toString().trim();
 
@@ -28,7 +13,6 @@ if (!versionTag || !/^version_(\d+)_([\d]+)_([\d]+)$/.test(versionTag)) {
 const newVersion = versionTag.replace('version_', '').replace(/_/g, '.');
 const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf-8'));
 
-
 if (!(packageJson.version === newVersion)) {
   packageJson.version = newVersion;
   fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2) + '\n', 'utf-8');
@@ -36,4 +20,3 @@ if (!(packageJson.version === newVersion)) {
   execSync(`git commit -m "Update package.json version to ${newVersion}"`);
   console.log(`updated package.json version to ${newVersion}`);
 }
-
